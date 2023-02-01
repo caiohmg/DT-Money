c686e8dc552add0f2c823ce54bfdc6252195ba7d
caiohmg <108705024+caiohmg@users.noreply.github.com>
2023-02-01 10:44:42 -0300

    .

--- src/componenets/NewTransactionModal/index.tsx
+++ src/componenets/NewTransactionModal/index.tsx
@@ -2,14 +2,14 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { ArrowCircleUp, X, ArrowCircleDown } from 'phosphor-react';
 import { CloseButton, Content, Overlay, TransactionType, TransactioTypeButton, } from "./styles";
 import * as z from 'zod'
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js';
 
 const newTrasactionFormShema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  /*type: z.enum(['income', 'outcome']),*/
+  type: z.enum(['income', 'outcome']),
 })
 
 type newTransactionFormInputs = z.infer<typeof newTrasactionFormShema>;
@@ -17,6 +17,7 @@ type newTransactionFormInputs = z.infer<typeof newTrasactionFormShema>;
 export function NewTransactionModal() {
 
     const {
+      control,
       register,
       handleSubmit,
       formState: { isSubmitting},
@@ -39,7 +40,7 @@ export function NewTransactionModal() {
         <CloseButton>
           <X size={24}/>
         </CloseButton>
-
+      
         <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
           <input type="text"
             placeholder="Descrição" 
@@ -57,8 +58,14 @@ export function NewTransactionModal() {
             required
             {...register ('category')}
             />
-
-          <TransactionType>
+            
+          <Controller
+          control={control}
+          name="type"
+          render={({ field }) => {
+            
+            return (
+              <TransactionType onValueChange={field.onChange} value={field.value}>
             <TransactioTypeButton variant="income" value="income">
               <ArrowCircleUp size={24} />
               Entrada
@@ -71,6 +78,9 @@ export function NewTransactionModal() {
             </TransactioTypeButton>
 
           </TransactionType>
+            )
+          }}
+          />
 
           <button type="submit" disabled={isSubmitting}>
             Cadastrar
--- src/contexts/TransactionsContexts.tsx
+++ src/contexts/TransactionsContexts.tsx
@@ -1,5 +1,9 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
+
+
+
+
 interface Transaction {
   id: number;
   description: string;
@@ -11,6 +15,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -21,20 +26,30 @@ export const TransactionContext = createContext({} as TransactionContextType);
 
 export function TransactionProvider({children}: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3333/transactions")
+
+    if (query) {
+      url.searchParams.append('q', query)
+    }
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
 return (
-  <TransactionContext.Provider value={{ transactions }}>
+  <TransactionContext.Provider value={{
+     transactions,
+     fetchTransactions,
+  }}>
     {children}
   </TransactionContext.Provider>
 )
 }
+
+
